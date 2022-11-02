56ca47770d7b6b792728e91e642e13faadfee3fd
Jeon Niroumand <jeon.niroumand@gmail.com>
2022-11-01 18:34:40 -0700

    added brand board and made come color changes

---
+++ menu.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.getElementById("hamburger");
+const logo = document.getElementById("logo");
+
+
+hamburger.addEventListener("click", alert("clicky"));
+
+/*function toggleMenu() {
+    if (menu.classList.contains("shwoMenu")) {
+        menu.classList.remove("showMenu");
+    }
+    else {
+        menu.classList.add("showMenu");
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);*/
