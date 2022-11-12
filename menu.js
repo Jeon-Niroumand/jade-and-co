408a60e262a2efc013aa073322f25b035713c2db
Jeon Niroumand <jeon.niroumand@gmail.com>
2022-11-11 22:30:53 -0800

    Added contact page, changed social icons

--- menu.js
+++ menu.js
@@ -1,7 +1,9 @@
 function toggle() {
    menu = document.getElementById("menu")
+   hamburger = document.getElementById("hamburger")
    if(menu.style.display == 'block') {
     menu.style.display = 'none';
+    hamburger.style.display = 'none';
     }
     else {
         menu.style.display = 'block';
