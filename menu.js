202102ff6415ebfd436d37fb46a8e2b27471ba85
Jeon Niroumand <jeon.niroumand@gmail.com>
2022-11-12 11:55:03 -0800

    made menu.js better with done button

--- menu.js
+++ menu.js
@@ -1,11 +1,15 @@
 function toggle() {
    menu = document.getElementById("menu")
    hamburger = document.getElementById("hamburger")
+   done = document.getElementById("done")
    if(menu.style.display == 'block') {
     menu.style.display = 'none';
-    hamburger.style.display = 'none';
+    done.style.display = 'none';
+    hamburger.style.display = 'block';
     }
     else {
         menu.style.display = 'block';
+        hamburger.style.display = 'none';
+        done.style.display = 'block'
     }
 }
